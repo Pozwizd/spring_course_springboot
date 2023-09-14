3884557c644e3dbbbde1ab78e4a2cbed6872deaa
pozwizd <miahkykh.roman@gmail.com>
2023-09-14 12:29:17 +0300

    -

---
+++ src/main/java/com/pozwizd/spring_course_springboot/dao/EmployeeDAOImpl.java
@@ -0,0 +1,46 @@
+package com.pozwizd.spring_course_springboot.dao;
+
+
+import com.pozwizd.spring_course_springboot.entity.Employee;
+import jakarta.persistence.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> allEmployees = query.getResultList();
+
+        return allEmployees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Query query = entityManager.createQuery("delete from Employee where id = :employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
+
+}
